193db37c4d8267cd3d57fa9e058c3445aecff2b2
Suus ten Voorde <suus.ten.voorde@hva.nl>
2018-05-12 15:48:00 +0200

    drinks and toilet use animation

--- static/js/waves.js
+++ static/js/waves.js
@@ -1,12 +1,77 @@
-var cnt=document.getElementById("count");
-var water=document.getElementById("water");
-var percent=cnt.innerText;
-var interval;
-interval=setInterval(function(){
+var cnt = document.getElementById("count");
+var water = document.getElementById("water");
+var progress = document.querySelector(".progress");
+var percent = cnt.innerText;
+
+//interval
+var interval1;
+var interval2;
+var milisec = 120;
+
+//translate values
+var x = 0;
+var y;
+
+//change values of drinks/toilets here
+var drinks = 32;
+var toilets = 8;
+var dashoffset = 339.292 * (1 - (drinks/100));
+
+//load drinks
+interval1 = setInterval(function(){
+	//keyframe
+	getRule(dashoffset);
+
+	//speed animation
+	var sec = (milisec / 1000) * drinks;
+	progress.style.animation = "progress " +  sec + "s linear forwards";
+
+	//how many drinks
   percent++;
-  cnt.innerHTML = percent + ' drankjes';
-  water.style.transform='translate(0'+','+(100-percent)+'%)';
-  if(percent == 32){
-    clearInterval(interval);
+  cnt.innerHTML = percent + ' drinks';
+	y = 100 - percent;
+  water.style.transform = 'translate(' + x + ',' + y +'%)';
+  if(percent == drinks){
+    clearInterval(interval1);
   }
-},120);
+}, milisec);
+
+setTimeout(function(){
+	interval2 = setInterval(function(){
+		//keyframe
+		var dashoffsetToilet = 339.292 * (1 - (toilets/100));
+		progress.style.strokeDashoffset = dashoffset;
+
+		var sec = ((milisec / 1000) * drinks) - toilets;
+		progress.style.animationPlayState = "running";
+		progress.style.animation = "progress " +  sec + "s linear forwards";
+
+		getRule(dashoffsetToilet);
+
+		percent--;
+		cnt.innerHTML = percent + ' toilet';
+		var y2 = 100 - percent;
+		water.style.transform = 'translate(' + x + ',' + y2 +'%)';
+		if(percent == toilets){
+			clearInterval(interval2);
+		}
+	}, milisec);
+}, 8000);
+
+
+// change keyframes in javascript
+var cssRule;
+function getRule(dashoffset) {
+	var rule;
+	var ss = document.styleSheets;
+
+	for (var i = 0; i < ss.length; ++i) {
+		for (var x = 0; x < ss[i].cssRules.length; ++x) {
+			rule = ss[i].cssRules[x];
+			if (rule.name == "progress" && rule.type == CSSRule.KEYFRAMES_RULE) {
+				cssRule = rule;
+				cssRule.appendRule("100% { stroke-dashoffset:" + dashoffset + "; }");
+			}
+		}
+	}
+}
