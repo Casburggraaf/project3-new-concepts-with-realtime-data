d22a97c9e7f8f8f36d7c65e97c32fdb1139575ea
Cas Burggraaf <casburggraaf@gmail.com>
2018-05-14 16:29:02 +0200

    added calc function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var SockJS = require('sockjs-client-node');
 var Stomp = require('stompjs');
+var MongoClient = require('mongodb').MongoClient;
 
 express()
   .use(express.static('static'))
@@ -11,7 +12,8 @@ express()
   .listen(3010)
 
 function index(req, res) {
-  res.render('index.ejs', {data: data});
+  let usage = engergy.getUsage();
+  res.render('index.ejs', {data: usage});
 }
 
 const stomp = {
@@ -27,10 +29,83 @@ const stomp = {
 
     stomp.client.subscribe("/exchange/power/C0", stomp.onData);
   },
-  onData(d){
-    console.log(d);
-    stomp.data.push(d)
+  onData(data){
+    let json = JSON.parse(data.body);
+    
+    let parseData = {
+      time: Date.now(),
+      solar: json.solar,
+      consumption: json.consumption
+    };
+
+   parseData.time = new Date(parseData.time);
+   parseData.time.setSeconds(0);
+   parseData.time.setMilliseconds(0);
+   parseData.time = parseData.time.getTime();
+
+    mongo.db.collection("data").insertOne(parseData, function(err, res) { 
+      if (err) throw err;
+    });
   }
 };
 
-stomp.init()
+
+const engergy = {
+  init(){
+
+  },
+  getUsage() {
+    let temp = {
+      last: null,
+      first: null,
+      total: {
+        time: null,
+        consumption: null,
+        solar: null
+      }
+    };
+
+    this.getUsageDataFirst().then((dataFirst) => {
+      temp.first = dataFirst;
+
+      this.getUsageDataLast().then((dataLast) => {
+        temp.last = dataLast;
+        temp.total.time = temp.last.time - temp.first.time;
+        temp.total.consumption = temp.last.consumption - temp.first.consumption;
+        temp.total.solar = temp.last.solar - temp.first.solar;
+        return temp;
+      });
+    });
+  },
+  getUsageDataFirst(){
+    return new Promise((resolve, reject) => {
+      let query = {time: Date.parse('14 May 2018 15:33')}
+      mongo.db.collection("data").find(query).limit(10).toArray(function(err, result) {
+        if (err) throw err;
+        resolve(result[0]);
+      });  
+    });
+  },
+  getUsageDataLast(){
+    return new Promise((resolve, reject) => {
+      let query = {time: Date.parse('14 May 2018 15:43')}
+      mongo.db.collection("data").find(query).limit(10).toArray(function(err, result) {
+        if (err) throw err;
+        resolve(result[0]);
+      });  
+    });
+  }
+};
+
+
+const mongo = {
+  db: null,
+  init(){
+    MongoClient.connect('mongodb://localhost/ceuvel', (err, client) => {
+      stomp.init()
+      this.db = client.db('ceuvel');
+    });
+  }
+};
+
+mongo.init();
